93a22e5c56f02617e2294dbd787a9758226a731a
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-06-12 15:31:02 +0900

    feat: wip

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,11 @@
-use std::io;
+use rand::Rng;
+use std::{io, cmp::Ordering};
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -10,6 +14,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+        
+    let guess: u32 = guess.trim().parse().expect("pls type a number!");
 
     println!("You guessed: {}", guess);
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
