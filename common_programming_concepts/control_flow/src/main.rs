bc23d478e54bfee2fc9025e3de1a7abf9c0cd47d
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-07-16 18:25:03 +0900

    feat: rect

---
+++ common_programming_concepts/control_flow/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    err();
+}
+
+
+fn err() {
+    let number = 3;
+
+    if number!=0 {
+        println!("number was not 0 three");
+     }
+}
