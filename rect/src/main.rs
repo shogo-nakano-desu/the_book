979883e5261ab978d413d4abed1e3795a77a75e9
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-07-17 21:40:37 +0900

    feat: 5.3 method

--- rect/src/main.rs
+++ rect/src/main.rs
@@ -4,19 +4,38 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> u32 {
+        self.width 
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-    println!("{:#?}",rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    if rect1.width()>0 {
+        println!(
+            "The area of the rectangle is {} square pixels.",
+            rect1.area()
+        );
+    }
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
