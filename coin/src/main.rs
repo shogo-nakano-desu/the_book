03348dc76bc7b1d7462e2cda816a617d874ac896
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-07-20 21:08:54 +0900

    feat: 6.3 if let

--- coin/src/main.rs
+++ coin/src/main.rs
@@ -13,6 +13,16 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Quarter => 25,
     }
 }
+
+fn if_let_ex(ex: Option<u8>) {
+    let config_max = ex;
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+}
+
 fn main() {
     println!("{}", value_in_cents(Coin::Penny));
+    //    println!("{:?}", if_let_ex(Some(2u8)));
+    if_let_ex(Some(3u8));
 }
