e73fd4ffb735542115d3e1dd9b49cd92ea02977b
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-07-23 15:26:15 +0900

    feat: 8.3 hashmap

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,4 +1,24 @@
 fn main() {
+    use std::collections::HashMap;
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
     let mut v = vec![1, 2, 3, 4, 5];
 
     let first = &mut v[0];
