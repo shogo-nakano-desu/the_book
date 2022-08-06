7a38bd9f6d99a09d6e21037c1cdeaf071cde0203
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-08-06 11:11:13 +0900

    feat: 10.3 lifetime

--- guessing_game/largest/src/main.rs
+++ guessing_game/largest/src/main.rs
@@ -1,4 +1,4 @@
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -13,11 +13,11 @@ fn largest<T>(list: &[T]) -> T {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest::<i32>(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest::<char>(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
